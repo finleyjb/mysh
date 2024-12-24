271adf8fb6f784356cff40a50d6078c5f93f4266
Finley Baker <finjothegreat@gmail.com>
2024-12-24 12:26:01 -0300

    Add handling for Ctrl-C

--- main.ts
+++ main.ts
@@ -1,31 +1,43 @@
 import { verbose } from "./src/log.ts";
 import { lex } from "./src/parse.ts";
 
+Deno.addSignalListener("SIGINT", () => {
+  console.log("interrupted!");
+});
+
 if (import.meta.main) {
   while (true) {
     const command = prompt(`${Deno.cwd()}$`);
-    setTimeout(() => {}, 5000);
-    if (!command) {
-      verbose("Empty input, exiting");
-      Deno.exit(0);
-    }
-    const commandParts = lex(command);
+    // This promise weirdness is necessary because prompting in a loop would 
+    // otherwise prevent handling SIGTERM
+    let promise = new Promise((resolve) => {
+      setTimeout(async () => {
+        if (!command) {
+          verbose("Empty input, exiting");
+          resolve(null);
+          return;
+        }
+        const commandParts = lex(command);
 
-    const childProc = new Deno.Command(commandParts[0], {
-      args: commandParts.slice(1),
+        const childProc = new Deno.Command(commandParts[0], {
+          args: commandParts.slice(1),
+        });
+        let stdout: Uint8Array | undefined;
+        try {
+          ({ stdout } = await childProc.output());
+        } catch (err) {
+          verbose(err);
+          if ((err as { code?: string }).code === "ENOENT") {
+            console.error(`mysh: command not found: ${commandParts[0]}`);
+          }
+        }
+        if (!stdout) {
+          resolve(null);
+          return;
+        }
+        console.log(new TextDecoder().decode(stdout));
+      }, 0);
     });
-    let stdout: Uint8Array | undefined;
-    try {
-      ({ stdout } = await childProc.output());
-    } catch (err) {
-      verbose(err);
-      if (err.code === "ENOENT") {
-        console.error(`mysh: command not found: ${commandParts[0]}`);
-      }
-    }
-    if (!stdout) {
-      continue;
-    }
-    console.log(new TextDecoder().decode(stdout));
+    await promise;
   }
 }
