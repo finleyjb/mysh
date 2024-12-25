ba3f270372831925cd52c14f0250e966c72892fb
Finley Baker <finjothegreat@gmail.com>
2024-12-25 09:28:59 -0300

    Use node readline API for getting user input

--- main.ts
+++ main.ts
@@ -1,43 +1,44 @@
 import { verbose } from "./src/log.ts";
 import { lex } from "./src/parse.ts";
+import * as readline from "node:readline/promises";
+import process from "node:process";
 
-Deno.addSignalListener("SIGINT", () => {
-  console.log("interrupted!");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.addListener("SIGINT", () => {
+  console.log("\ninterrupted!\n");
 });
 
 if (import.meta.main) {
   while (true) {
-    const command = prompt(`${Deno.cwd()}$`);
-    // This promise weirdness is necessary because prompting in a loop would 
+    const command = await rl.question(`${Deno.cwd()}$ `);
+    console.log(command);
+    // This promise weirdness is necessary because prompting in a loop would
     // otherwise prevent handling SIGTERM
-    let promise = new Promise((resolve) => {
-      setTimeout(async () => {
-        if (!command) {
-          verbose("Empty input, exiting");
-          resolve(null);
-          return;
-        }
-        const commandParts = lex(command);
+    if (!command) {
+      verbose("Empty input, exiting");
+      break;
+    }
+    const commandParts = lex(command);
 
-        const childProc = new Deno.Command(commandParts[0], {
-          args: commandParts.slice(1),
-        });
-        let stdout: Uint8Array | undefined;
-        try {
-          ({ stdout } = await childProc.output());
-        } catch (err) {
-          verbose(err);
-          if ((err as { code?: string }).code === "ENOENT") {
-            console.error(`mysh: command not found: ${commandParts[0]}`);
-          }
-        }
-        if (!stdout) {
-          resolve(null);
-          return;
-        }
-        console.log(new TextDecoder().decode(stdout));
-      }, 0);
+    const childProc = new Deno.Command(commandParts[0], {
+      args: commandParts.slice(1),
     });
-    await promise;
+    let stdout: Uint8Array | undefined;
+    try {
+      ({ stdout } = await childProc.output());
+    } catch (err) {
+      verbose(err);
+      if ((err as { code?: string }).code === "ENOENT") {
+        console.error(`mysh: command not found: ${commandParts[0]}`);
+      }
+    }
+    if (!stdout) {
+      continue;
+    }
+    console.log(new TextDecoder().decode(stdout));
   }
 }
